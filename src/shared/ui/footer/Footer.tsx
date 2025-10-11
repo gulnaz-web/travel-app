8974bc79a107ab563a4cceb87b75033cd5bc8227
Yeti <gulnaz.davletshina.97@mail.ru>
2025-10-11 11:12:14 +0300

    added: block Footer

---
+++ src/shared/ui/footer/Footer.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
+import styles from './Footer.module.scss'
+
+const navItems = [
+  {
+    icon: 'home',
+    link: '/',
+  },
+  {
+    icon: 'explore',
+    link: '/explore',
+  },
+  {
+    icon: 'place',
+    link: '/place',
+  },
+  {
+    icon: 'person_outline',
+    link: '/user',
+  },
+]
+
+export const Footer = () => {
+  const pathname = usePathname()
+
+  return (
+    <footer className={styles.footer}>
+      <nav className={styles.nav}>
+        {navItems.map((item) => (
+          <Link 
+            href={item.link}
+            className={`${styles.link} ${pathname === item.link ? styles.active : ''}`}
+            key={item.icon}
+          >
+            <span className="material-icons-outlined">{item.icon}</span>
+          </Link>
+        ))}
+      </nav>
+    </footer>
+  )
+}
